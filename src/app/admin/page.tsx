be39d07bc897a708c993d137026040480e8a592e
Mikel mart <mikelmart79@gmail.com>
2026-05-04 17:22:27 +0200

    Añadir gestion de grupos al admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -39,17 +39,16 @@ export default function AdminPage() {
             </Link>
 
             <Link
-              href="/admin/equipos"
-              className="rounded-2xl bg-white/95 p-5 text-lg font-black shadow"
-            >
-
-	    <Link
               href="/admin/grupos"
               className="rounded-2xl bg-white/95 p-5 text-lg font-black shadow"
             >
+              Gestionar grupos
+            </Link>
 
-  Gestionar grupos
-</Link>
+            <Link
+              href="/admin/equipos"
+              className="rounded-2xl bg-white/95 p-5 text-lg font-black shadow"
+            >
               Gestionar equipos
             </Link>
 

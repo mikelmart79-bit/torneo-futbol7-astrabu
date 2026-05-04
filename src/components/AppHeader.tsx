e8db8eb18f941678a01d750343f3ac7645f82ae0
Mikel mart <mikelmart79@gmail.com>
2026-05-04 10:07:12 +0200

    Ajustar cabecera en una linea

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,7 +2,7 @@ export default function AppHeader() {
   return (
     <header className="fixed left-0 top-0 z-50 w-full bg-black/85 px-4 py-3 text-center text-white shadow-xl backdrop-blur">
       <h1 className="text-lg font-black text-center whitespace-nowrap sm:text-xl">
-        Torneo Fútbol 7 Astrabudua
+        Torneo Fútbol 7 Astrabu
       </h1>
     </header>
   );

af3aea7df4979d25c1365e2dafd5c3e44a117664
Mikel mart <mikelmart79@gmail.com>
2026-07-08 11:32:25 +0200

    Quitar titulo inicio de cabecera

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -293,8 +293,6 @@ export default function InicioPage() {
           >
             Torneo Fútbol 7 Astrabudua
           </p>
-
-          <h1 className="mt-2 text-center text-3xl font-black">Inicio</h1>
         </div>
 
         <div className="mt-4 overflow-hidden rounded-3xl bg-white/95 shadow-2xl backdrop-blur">
